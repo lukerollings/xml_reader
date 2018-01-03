bf4472b6b99c8e898e4e3df16c9b9dfa83718184
lukerollings <33325701+lukerollings@users.noreply.github.com>
2018-01-03 15:02:58 +0000

    Add files via upload
    produces arrays of x coordinates and y coordinates, inc. scatter graph for confirmation

--- xml_reader.py
+++ xml_reader.py
@@ -6,15 +6,21 @@ Created on Wed Jan  3 12:19:42 2018
 """
 
 import os
+import numpy as np, matplotlib.pyplot as plt
 import xml.etree.ElementTree as ET
 import glob
 
 os.chdir('C:\\Users\\mbgnwlr2\\Documents\\PhD_LabStuff\\Radiography_17_12_20')
 
-print('x axis position (mm)', 'y axis position (mm)', 'magnification (mm)', 'rotation (degrees)', 'tilt (degrees)', 'imaging (mm)') 
+print('image', 'x axis position (mm)', 'y axis position (mm)', 'magnification (mm)', 'rotation (degrees)', 'tilt (degrees)', 'imaging (mm)') 
 
 filenames = sorted(glob.glob('AlSiC_monosheet_*.xml'))
-filenames = filenames[0:100]
+filenames = filenames[0:96]
+
+np.xpos = [] #list to add x coordinates
+np.ypos = [] #list to add y coordiantes
+
+i = 0 #iteration counter
 
 for f in filenames:
 
@@ -29,6 +35,13 @@ for f in filenames:
     tilt = float(root[5][4].text)
     img = float(root[5][5].text)
 
+    np.xpos.insert(i, x1)
+    np.ypos.insert(i, y1)
+    
+    i = i+1
 
 
-    print ('%-21i%-21i%-19i%-19i%-15i%-13i' % (x1, y1, mag, rot, tilt, img))
+    print ('%-6i%-21i%-21i%-19i%-19i%-15i%-13i' % (i, x1, y1, mag, rot, tilt, img))
+    
+    
+plt.scatter(np.xpos, np.ypos, s=1)
